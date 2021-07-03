ffa6fe34496fc85acf56fbcbc3321e2233f8a19f
nishatbristy007 <1505007.nab@ugrad.cse.buet.ac.bd>
2021-07-04 05:23:12 +0600

    Added: Simulate from a base genome.

--- simulate.py
+++ simulate.py
@@ -6,7 +6,7 @@ from scipy.linalg import expm
 import random
 from os.path import join
 from os import makedirs
-
+import readfq as rfq
 
 # $1 is config file
 # $2 is output directory
@@ -17,6 +17,43 @@ def rc(seq):
 
 def hamming(seq1,seq2):
 	return 1- sum([x==y for x,y in zip(seq1,seq2)])/len(seq1)
+
+def findOccurrences(scafno,string_,substring_):
+    return [(scafno,i) for i, letter in enumerate(string_) if letter == substring_]
+
+
+def params(w,pac,pag,pat,length,pi):
+	# A C G T
+	aux=w/(1-w)
+
+	# TK4 rate matrix
+	R = np.array([[-(pac+pag+pat) ,pac ,pag, pat],
+		[pac*aux, -(pac*aux+pat+pag*aux), pat ,pag*aux],
+		[pag*aux, pat, -(pag*aux+pat+pac*aux) ,pac*aux],
+		[pat, pag, pac,-(pat+pag+pac)]])
+
+	
+
+	# normalize the rate matrix
+	nrm = -np.dot(np.diag(R),pi)
+	Rn = R/nrm
+
+	# matrix exponentiation
+	Pm = expm(t*Rn)
+	# hamming distance
+	Hd = 1-(np.dot(np.diag(Pm),pi))
+	# jukes-cantor phylogenetic distance
+	Jc = -3/4*np.log(1-4/3*Hd)
+
+	# expected substitution probabilities
+	F = (Pm.T*pi).T
+
+	# expected number of each type of substition
+	# in a genome with length "length"
+	Flen = np.ceil(length*F)
+	#print(Flen)
+	return Jc, Hd, F, Pm, R, Flen
+
 with open(sys.argv[1]) as f:
 	j = json.load(f)
 
@@ -25,9 +62,8 @@ with open(sys.argv[1]) as f:
 pag=j["alpha"]
 pac=j["delta"]
 pat=j["gamma"]
-w=j["omega"]
 t=j["t"]
-length=j["length"]
+gen = 0
 
 if "scaffolds" in j:
 	numscaf = j["scaffolds"]
@@ -40,54 +76,83 @@ else:
 	seed=12345
 random.seed(seed)
 
-# A C G T
-aux=w/(1-w)
 
-# TK4 rate matrix
-R = np.array([[-(pac+pag+pat) ,pac ,pag, pat],
-	[pac*aux, -(pac*aux+pat+pag*aux), pat ,pag*aux],
-	[pag*aux, pat, -(pag*aux+pat+pac*aux) ,pac*aux],
-	[pat, pag, pac,-(pat+pag+pac)]])
+if "genome" in j:
+	ref_genome=j["genome"]
+	gen = 1
+	f = open(ref_genome)
+	n, slen, qlen = 0, 0, 0
+	scafs = []
+	idx_occurances = []				# List of tuples (scaf.no, index of letter).
 
-pi = np.array([w/2, 1/2-w/2, 1/2-w/2, w/2])
+	for i in range(len(["A","C", "G","T"])):
+		idx_occurances.append([])
 
-# normalize the rate matrix
-nrm = -np.dot(np.diag(R),pi)
-Rn = R/nrm
+	for name, seq, qual in rfq.readfq(f):
+		l = len(seq)
+		#print(seq)
+		slen += l
+		for ind_i,i in enumerate(["A","C", "G","T"]):
+			idx_occurances[ind_i].extend(findOccurrences(n,seq,i))
+		scafs.append([(i,i) for i in seq])
+		n += 1
 
-# matrix exponentiation
-Pm = expm(t*Rn)
-# hamming distance
-Hd = 1-(np.dot(np.diag(Pm),pi))
-# jukes-cantor phylogenetic distance
-Jc = -3/4*np.log(1-4/3*Hd)
+	w = 2*len(idx_occurances[0])/slen
+	pi = np.array([len(i)/slen for i in idx_occurances])
+	#print(pi)
 
-# expected substitution probabilities
-F = (Pm.T*pi).T
+	length = slen
+	Jc, Hd, F, Pm, R, Flen = params(w,pac,pag,pat,length,pi)
+	#print(scafs)
+	#print(idx_occurances)
 
-# expected number of each type of substition
-# in a genome with length "length"
-Flen = np.ceil(length*F)
+	#shuffle the sites 
+	for i in idx_occurances:
+		random.shuffle(i)
+	#print(idx_occurances)
 
-# create two genomes as list of pairs
-genome_pairs = []
-for ind_i,i in enumerate(["A","C", "G","T"]):
-	for ind_j,j in enumerate(["A","C", "G","T"]):
-		genome_pairs +=  [(i,j)]*int(Flen[ind_i,ind_j])
+	for ind_i,i in enumerate(["A","C", "G","T"]):
+		start=0
+		for ind_j,j in enumerate(["A","C", "G","T"]):
+			for k in range(int(Flen[ind_i,ind_j])):
+				if start + k >= len(idx_occurances[ind_i]):
+					continue
+				# idx_occurances[ind_i][start+k][0] contains the scaffold number.
+				# idx_occurances[ind_i][start+k][1] contains the index of the character in the scaffold.
+				scafs[idx_occurances[ind_i][start+k][0]][idx_occurances[ind_i][start+k][1]] = (i,j)
+				
+			start += int(Flen[ind_i,ind_j])
+	#print(scafs)
+else:
+	w=j["omega"]
+	length=j["length"]
+	pi = np.array([w/2, 1/2-w/2, 1/2-w/2, w/2])
 
-# shuffle the sites
-random.shuffle(genome_pairs)
+	Jc, Hd, F, Pm, R, Flen = params(w,pac,pag,pat,length, pi)
+	# create two genomes as list of pairs
+	genome_pairs = []
+	for ind_i,i in enumerate(["A","C", "G","T"]):
+		for ind_j,j in enumerate(["A","C", "G","T"]):
+			genome_pairs +=  [(i,j)]*int(Flen[ind_i,ind_j])
 
-# partition into equal length scaffolds
-scafsize = len(genome_pairs)//numscaf
-scafs = []
-for i in range(numscaf-1):
-	scafs.append(genome_pairs[scafsize*i:scafsize*(i+1)])
-scafs.append(genome_pairs[scafsize*(numscaf-1):])
+	#print(genome_pairs)
 
+	# shuffle the sites
+	random.shuffle(genome_pairs)
+
+	# partition into equal length scaffolds
+	scafsize = len(genome_pairs)//numscaf
+	scafs = []
+	for i in range(numscaf-1):
+		scafs.append(genome_pairs[scafsize*i:scafsize*(i+1)])
+	scafs.append(genome_pairs[scafsize*(numscaf-1):])
+
+	#print(genome_pairs)
+	#print("scafs: ",scafs)
 count = 0
-for i in scafs:
-	count += len(i)
+for scaf in scafs:
+	count += len(scaf)
+#print(count)
 
 # for each scaffold, flip g1 and g2 with %50 probability
 flipped_scaf = []
@@ -97,7 +162,8 @@ for sc in scafs:
 	flip1 = random.randint(0,1)
 	flip2 = random.randint(0,1)
 	if flip1:
-		seq1 = rc(seq1)
+		if gen == 0:
+			seq1 = rc(seq1)
 	if flip2:
 		seq2 = rc(seq2)
 	flipped_scaf.append((seq1,seq2))
